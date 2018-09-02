374fa55b61072df7981e7e0bc9221ec23293e805
dennisranish <dennisranish@gmail.com>
2018-09-02 01:58:45 -0400

    correct url

--- TestWebApp/app.js
+++ TestWebApp/app.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator)
 {
     window.addEventListener('load', function()
     {
-      navigator.serviceWorker.register('/sw.js').then(function(registration)
+      navigator.serviceWorker.register('https://dennisranish.github.io/Controller/TestWebApp/sw.js').then(function(registration)
       {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
