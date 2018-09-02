77e6bff84d48c0b7cac2b0c4a78d2c44ba53954b
dennisranish <dennisranish@gmail.com>
2018-09-02 03:44:30 -0400

    Test lock orientation

--- TestWebApp/app.js
+++ TestWebApp/app.js
@@ -2,14 +2,16 @@ if ('serviceWorker' in navigator)
 {
     window.addEventListener('load', function()
     {
-      navigator.serviceWorker.register('https://dennisranish.github.io/Controller/TestWebApp/sw.js').then(function(registration)
-      {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err)
-      {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
+        navigator.serviceWorker.register('https://dennisranish.github.io/Controller/TestWebApp/sw.js').then(function(registration)
+        {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err)
+        {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  }
+}
+
+screen.orientation.lock('landscape');
